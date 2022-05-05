e404609827431aa91502ef91edda46cb7a5713f8
Debajyoti Dutta <debajyotidutta.50@gmail.com>
2022-05-05 23:07:50 +0530

    update app1 url

--- src/main/java/com/debu/ms/app2/app1Proxy.java
+++ src/main/java/com/debu/ms/app2/app1Proxy.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 //@FeignClient(name="app1ks")
-@FeignClient(name="app1ks",url="${APP2KS_HOST:http://localhost}:8000")
+@FeignClient(name="app1ks",url="${APP1KS_SERVICE_HOST:http://localhost}:8000")
 public interface app1Proxy {
 	@GetMapping("/")
 	public app1Response getApp1Response();
